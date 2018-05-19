0fc363a63062ea6d21d37f400f894a19085ccd7e
Mottaqui Karim <taqqui.karim@gmail.com>
2018-05-19 05:29:22 -0400

    adding offline support, on initial app load fetch and catch todays data and next 3 days data

--- javascript/app.js
+++ javascript/app.js
@@ -2,23 +2,16 @@
 
     const APP_ENTRY = '.js-app';
     const APP_EL = document.querySelector(APP_ENTRY);
-    const fillContainer = (markup) => {
-        APP_EL.innerHTML = markup;
-    }
+    const fillContainer = markup => APP_EL.innerHTML = markup;
 
     // essentially, we want to store date object representing TODAY
     // but not NOW. this is used as a key for caching responses in localstorage
-    let now = new Date();
-    now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    const getFormattedDate = () => {
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec",]
-        const month = months[now.getMonth()]
+    const _time_now = new Date();
+    let now = new Date(_time_now.getFullYear(), _time_now.getMonth(), _time_now.getDate());
 
-        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]
-        const day = days[now.getDay()];
-
-        return `${day} ${month} ${now.getDate()}, ${now.getFullYear()}`
-    }
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const getFormattedDate = () => `${days[now.getDay()]} ${months[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()}`
 
     const templates = {
         LOAD_SVWORKER: `
@@ -66,7 +59,16 @@
                     Or, view data for <a href="#" class="js-nyc" style="font-weight: bold;">New York City, US</span>.
                 <p>
             </div>
-        `
+        `,
+        OFFLINE: `
+            <div class="app-container js-geo-perm">
+                <span class="oi oi-ban large-icon"></span>
+                <p style="margin-bottom: 0; width: 70%;">
+                    Data not available offline! Please go online and try again.
+                    To go back to last page, please click here <a href="./index.html" style="font-weight: bold;">here</a>.
+                </p>
+            </div>
+        `,
     }
 
     const queryGeolocation = () => new Promise((resolve, reject) => {
@@ -74,15 +76,15 @@
         const timeout = setTimeout(_ => {
             fillContainer(templates.STILL_WORKING);
         }, 3000);
+
         navigator.geolocation.getCurrentPosition(position => {
             resolve({
                 lat: position.coords.latitude,
                 lon: position.coords.longitude,
                 timeout,
             })
-        }, err => {
-            reject({err, timeout}); 
-        });
+        }, err => reject({err, timeout}));
+
     }).then(({lat, lon, timeout}) => {
         const locStr = `${lat},${lon}`;
         clearTimeout(timeout)
@@ -112,7 +114,7 @@
         return queryGeolocation();
     });
 
-    const queryAPI = loc => new Promise((resolve, reject) => {
+    const queryAPI = (loc, dateObj=now) => new Promise((resolve, reject) => {
         let endpoint;
         if (localStorage.getItem('city-override')) {
             endpoint = `city/${localStorage.getItem('city-override')}`;
@@ -124,23 +126,21 @@
 
         // if found in localstorage cache, use that instead
         // TODO: periodically, localstorage should be cleared
-        const cachedData = localStorage.getItem(now.getTime()+'-'+endpoint)
+        const cachedData = localStorage.getItem(dateObj.getTime()+'-'+endpoint)
         if (cachedData) {
             resolve(JSON.parse(cachedData))
             return;
         }
 
         const urlBase = 'https://ksw1yk85j7.execute-api.us-east-1.amazonaws.com/prod';
-        const args = `date=${Math.floor(now.getTime()/1000)}`;
+        const args = `date=${Math.floor(dateObj.getTime()/1000)}`;
 
         const xhr = new XMLHttpRequest();
         xhr.addEventListener("load", e => {
-            localStorage.setItem(now.getTime()+'-'+endpoint, e.target.responseText)
+            localStorage.setItem(dateObj.getTime()+'-'+endpoint, e.target.responseText)
             resolve(JSON.parse(e.target.responseText))
         });
-        xhr.addEventListener("error", e => {
-            reject(e)
-        })
+        xhr.addEventListener("error", e => reject(e))
         xhr.open('GET', `${urlBase}/${endpoint}?${args}`);
         xhr.send();
     });
@@ -207,31 +207,54 @@
         `);
     }
     
-    const init = () => {
-        getLocation()
-            .then(loc => {
-                if (!loc) {
-                    return;
-                }
+    const init = (look_forward=true) => {
+        const getLocationPromise = getLocation()
+        const queryAPIPromise = getLocationPromise.then(loc => {
+            if (!loc) {
+                throw new Error('Location services explictly blocked')
+                return;
+            }
 
-                fillContainer(templates.QUERYING_API);
-                return queryAPI(loc)
-            })
-            .then(data => renderData(data))
+            fillContainer(templates.QUERYING_API);
+            return queryAPI(loc)
+        })
+        const renderDataPromise = queryAPIPromise.then(data => renderData(data))
+
+        renderDataPromise
             .catch(e => {
                 console.log(e)
-                fillContainer(templates.SOMETHING_WRONG);
+                if (!navigator.onLine) {
+                    fillContainer(templates.OFFLINE);
+                }
+                else {
+                    fillContainer(templates.SOMETHING_WRONG);
+                }
             });
+
+        Promise.all([getLocationPromise, queryAPIPromise, renderDataPromise])
+            .then(all => {
+                const [loc] = all
+
+                // #fireAndForget
+                if (look_forward) {
+                    const forward_queries = []
+                    for (let i = 1; i < 4; i++) {
+                        const nextDateObj = new Date(now.getTime() + (1000*60*60*24)*i)
+                        forward_queries.push(queryAPI(loc, nextDateObj));
+                    }
+                    Promise.all(forward_queries);
+                }
+            })
     }
 
     APP_EL.addEventListener('click', e => {
         if (e.target.matches('.js-left')) {
             now = new Date(now.getTime() - (1000*60*60*24));
-            init();
+            init(false);
         }
         else if (e.target.matches('.js-right')) {
             now = new Date(now.getTime() + (1000*60*60*24));
-            init();
+            init(false);
         }
         else if (e.target.matches('.js-nyc')) {
             e.preventDefault();
@@ -239,6 +262,13 @@
             init();
         }
     });
+
+    const offlineEl = document.querySelector('.js-offline-alert');
+    if (!navigator.onLine) {
+        offlineEl.style.display = 'block'
+    }
+    window.addEventListener('offline', e => offlineEl.style.display = 'block')
+    window.addEventListener('online', e => offlineEl.style.display = 'none')
 
     fillContainer(templates.LOAD_SVWORKER);
     if ('serviceWorker' in navigator) {
@@ -270,11 +300,13 @@
       .catch(function(error) {
         console.log('Registration failed: ', error);
         console.log('loading app anyways...')
+        clearTimeout(installTimeout)
         init();
       });
     }
     else {
         console.log('no svworker')
+        clearTimeout(installTimeout)
         init();
     }
 })();
