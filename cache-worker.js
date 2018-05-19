f599a7db30fcc387a6814b0dad63967beef7ac19
Mottaqui Karim <taqqui.karim@gmail.com>
2018-05-19 10:47:00 -0400

    testing

--- cache-worker.js
+++ cache-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'static-cache-v17';
+var CACHE_NAME = 'static-cache-v18';
 var urlsToCache = [
   '.',
   'index.html',
--- javascript/app.js
+++ javascript/app.js
@@ -452,7 +452,7 @@ END:VCALENDAR`
 
             const a = document.createElement('a')
             a.innerHTML = `<div class="btn btn-success">Download Events</div>`
-            window.location.href = 'https://mottaquikarim.github.io/Ramadan2018/dd.html#data:text/calendar;charset=utf-8,'+ encodeURIComponent(ICS)
+            window.open('https://mottaquikarim.github.io/Ramadan2018/dd.html#data:text/calendar;charset=utf-8,'+ encodeURIComponent(ICS))
             //a.target = "_blank"
             //a.download = 'events.ics'
             for (let i = 0; i < _.parentNode.children.length; i++) {
